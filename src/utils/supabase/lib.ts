e507445a274f62110c98c38811b019762f8dd625
Hermann Agossou <hermannagossou6@gmail.com>
2025-12-01 18:16:54 +0100

    fix: Trying to resolve Supabase type definitions to pass lint

--- src/utils/supabase/lib.ts
+++ src/utils/supabase/lib.ts
@@ -1,9 +1,23 @@
 import { createClient } from '@supabase/supabase-js';
+// import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-let supabase: ReturnType<typeof createClient> | null = null;
+// // Local definition for debugging
+// type Database = {
+//   public: {
+//     Tables: {
+//       submissions: {
+//         Row: any;
+//         Insert: any;
+//         Update: any;
+//       };
+//     };
+//   };
+// };
+
+let supabase: any = null;
 
 // fetch supabase inside the function // if not already fetched
-export const lazySupabase = () => {
+export const lazySupabase = (): any => {
   if (!supabase) {
     supabase = createClient(
       import.meta.env.VITE_SUPABASE_URL,
